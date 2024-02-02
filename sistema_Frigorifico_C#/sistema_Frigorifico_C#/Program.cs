f5f2f002aca5f83fdbf0eeefed844acacd4d74e2
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2024-02-02 08:33:23 -0300

    Criação das variáveis globais

--- sistema_Frigorifico_C#/sistema_Frigorifico_C#/Program.cs
+++ sistema_Frigorifico_C#/sistema_Frigorifico_C#/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace sistema_Frigorifico_C_
 {
-    internal class Program
+
+    public class Program
     {
+        //Variáveis globais, na qual vão ter seus valores modificados em diferentes escopos
+        double peso = 0, peso_Max = 0, peso_Min = 3000000000, peso_Velho = 0, peso_Novo = 0, peso_Total = 0, preco_arroba = 0;
+        int id_animal = 0, meses_animal = 0, mais_velho = 0, mais_novo = 0, id_MaisPesado = 0, id_MaisLeve = 0, meses_MaisPesado = 0, meses_MaisLeve = 0, meses_Total = 0, contador = 0;
         static void Main(string[] args)
         {
+
         }
     }
 }
