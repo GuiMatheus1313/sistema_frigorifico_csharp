1402f2aea2a847ca56c95de6e0a17356c907a541
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2024-02-02 09:27:16 -0300

    Classe Global com a variáveis double completas (parcial)

---
+++ sistema_Frigorifico_C#/sistema_Frigorifico_C#/Global_variable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_Frigorifico_C_
+{
+    public static class Global_variable
+    {
+        //Variáveis globais, na qual vão ter seus valores modificados em diferentes escopos
+        static double peso { get; set; }
+        static double peso_Max  {get; set;}
+
+        static double peso_Min { get; set; },
+        double peso_Velho = 0,
+        static double peso_Novo = 0,
+        static double peso_Total = 0,
+        static double preco_arroba = 0;
+
+
+
+
+
+        int id_animal = 0, meses_animal = 0, mais_velho = 0, mais_novo = 0, id_MaisPesado = 0, id_MaisLeve = 0, meses_MaisPesado = 0, meses_MaisLeve = 0, meses_Total = 0, contador = 0;
+    }
+}
