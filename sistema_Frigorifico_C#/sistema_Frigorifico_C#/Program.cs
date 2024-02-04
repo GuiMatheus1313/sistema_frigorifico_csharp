9f1e8283f442b4b746fd3c6c1b31772a0c05114d
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2024-02-04 16:43:34 -0300

    Criação loop do preco_arroba (com try/catch)

--- sistema_Frigorifico_C#/sistema_Frigorifico_C#/Program.cs
+++ sistema_Frigorifico_C#/sistema_Frigorifico_C#/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,37 @@ namespace sistema_Frigorifico_C_
     public class Program
     {
         //Variáveis globais, na qual vão ter seus valores modificados em diferentes escopos
-        double peso = 0, peso_Max = 0, peso_Min = 3000000000, peso_Velho = 0, peso_Novo = 0, peso_Total = 0, preco_arroba = 0;
+        double peso = 0, peso_Max = 0, peso_Min = 3000000000, peso_Velho = 0, peso_Novo = 0, peso_Total = 0; 
         int id_animal = 0, meses_animal = 0, mais_velho = 0, mais_novo = 0, id_MaisPesado = 0, id_MaisLeve = 0, meses_MaisPesado = 0, meses_MaisLeve = 0, meses_Total = 0, contador = 0;
 
         static void Main(string[] args)
         {
+            double preco_arroba = 0; 
             int escolha = 0;
 
-            Console.WriteLine("Digite o preço da arroba do boi: ");
+            while(true)
+            {
+                int decisao = 0; 
+                Console.WriteLine("Digite o preço da arroba do boi: ");
+                try
+                {
+                    preco_arroba = Convert.ToDouble(Console.ReadLine());
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Formato incorreto no preenchimento!") ;
+                    decisao = 1;
+                }
+                if (decisao == 0)
+                    break;
+                    
+                //if (GetType(escolha))
+            }
+
+            Console.WriteLine("Saiu do loop!");
+            //while(true)
+            Console.ReadLine();
               
         }
     }
